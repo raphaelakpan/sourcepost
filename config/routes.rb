9a528d61c30833b85c470a737d2aca12674daeb1
Raphael Ifiok Akpan <raphael.akpan@andela.com>
2018-05-01 06:12:59 +0100

    Setup Boostrap and style layouts and nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home', to: 'static_pages#home'
-  get 'pages/help', to: 'static_pages#help'
-  get 'pages/about', to: 'static_pages#about'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
+  # resources :users, only: [:new]
+  get '/signup', to: 'users#new', as: 'signup'
 
   root 'static_pages#home'
 end
