1aa1865fefdc0bf4ec2ed08da77972abd2af9e00
Raphael Akpan <raphael.akpan@andela.com>
2018-06-02 10:57:28 +0100

    Allow users to edit profile (#3)
    * Allow user to edit profile
    * Authorization - edit only your profile
    * Friendly redirect after authorization
    * Display all users with pagination
    * Admins can delete users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
+  def require_login
+    unless logged_in?
+      store_location
+      flash[:warning] = 'Please login to continue'
+      redirect_to login_path
+    end
+  end
+
   def not_found
     flash[:warning] = 'Requested page not found!'
     redirect_to root_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: :show
+  before_action :require_login, only: [:edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :is_current_user, only: [:edit, :update]
+  before_action :is_admin_user, only: [:destroy]
+
+  def index
+    @users = User.order(:created_at).paginate(paginate_params)
+  end
 
   def new
     @user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -21,6 +31,22 @@ class UsersController < ApplicationController
   def show
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your profile has been successfully updated!"
+      redirect_to @user
+    else
+      flash.now[:error] = "Form has one or more errors!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    flash[:success] = "#{@user.name} successfully deleted!"
+    redirect_back(fallback_location: root_path)
+  end
+
   private
 
   def set_user
@@ -29,5 +55,20 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def is_current_user
+    redirect_to edit_user_path(current_user) unless current_user? @user
+  end
+
+  def paginate_params
+    {
+      page: params[:page],
+      per_page: params[:size].to_i > 0 ? params[:size] : 10
+    }
+  end
+
+  def is_admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 end
